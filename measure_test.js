32f6529e821e146e1c3af5d5084b766603ad805c
Tero <tero.tolonen@adenova.fi>
2016-09-06 10:33:02 +0300

    new version of test

--- measure_test.js
+++ measure_test.js
@@ -1,31 +1,44 @@
 var txt = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu. Sed arcu lectus auctor vitae, consectetuer et venenatis eget velit. Sed augue orci, lacinia eu tincidunt et eleifend nec lacus. Donec ultricies nisl ut felis, suspendisse potenti. Lorem ipsum ligula ut hendrerit mollis, ipsum erat vehicula risus, eu suscipit sem libero nec erat. Aliquam erat volutpat. Sed congue augue vitae neque. Nulla consectetuer porttitor pede. Fusce purus morbi tortor magna condimentum vel, placerat id blandit sit amet tortor";
+var resPre = document.createElement("pre");
+document.body.appendChild(resPre);
+
+var res_str = "";
+
+var log = {
+	time : function(n) {
+		this.ms = (new Date()).getTime()
+	},
+	timeEnd : function(n) {
+		res_str += n +" "+((new Date()).getTime() - this.ms)+"ms\n"
+		resPre.innerHTML = res_str;
+	}
+}
 
 var run = function(fn, times, name) {
-	console.time("sameTextOneLine"+name)
+	log.time(name+" sameTextOneLine")
 	for(var i=0; i<times; i++) {
 		fn("Arial", 12, 400, "Hello");
 	}
-	console.timeEnd("sameTextOneLine"+name)
+	log.timeEnd(name+" sameTextOneLine")
 
-	console.time("sameTextMultiLine"+name)
+	log.time(name+" sameTextMultiLine")
 	for(var i=0; i<times; i++) {
 		fn("Arial", 12, 400, txt);
 	}
-	console.timeEnd("sameTextMultiLine"+name)	
+	log.timeEnd(name+" sameTextMultiLine")	
 
-	console.time("sameTextMultiLineVariableLength"+name)
+	log.time(name+" sameTextMultiLineVariableLength")
 	for(var i=0; i<times; i++) {
 		fn("Arial", 12, 400, txt.substring(0, i%600));
 	}
-	console.timeEnd("sameTextMultiLineVariableLength"+name)		
+	log.timeEnd(name+" sameTextMultiLineVariableLength")		
 
-	console.time("sameTextMultiLineVariableLengthVariableWidth"+name)
+	log.time(name+" sameTextMultiLineVariableLengthVariableWidth")
 	for(var i=0; i<times; i++) {
 		fn("Arial", 12, 200+i%200, txt.substring(0, i%600));
 	}
-	console.timeEnd("sameTextMultiLineVariableLengthVariableWidth"+name)			
+	log.timeEnd(name+" sameTextMultiLineVariableLengthVariableWidth")			
 }
-
 run(MeasureText, 10000, "dom");
 run(MeasureTextCanvas, 10000, "canvas");
 run(MeasureTextCached, 10000, "cached");
